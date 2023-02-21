6ba5c578b70f92dfe7cac054069f76993bd05e40
Jonathan Phair <jmphair@gmail.com>
2023-02-21 15:00:10 -0500

    add: props to Day List Items and fix error in jsconfig

---
+++ src/components/DayList.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+// PROPS NEEDED
+// days:Array a list of day objects (each object includes an id, name, and spots)
+// day:String the currently selected day
+// setDay:Function accepts the name of the day eg. "Monday", "Tuesday"

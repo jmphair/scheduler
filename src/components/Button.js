14f24f54d0aa0c9969408beb4987bbc3ec2292e6
Jonathan Phair <jmphair@gmail.com>
2023-02-21 12:56:53 -0500

    edit: change button component to render button element

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,12 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   return <button>{props.children}</button>;
 }
+
+// PROPS (from tuesday's exercises in storybook)
+// Base uses no props and is considered the default button
+// Confirm uses the confirm prop to apply the .button--confirm modifier class
+// Danger uses the danger prop to apply the .button--danger modifier class
+// Clickable uses the onClick prop to handle the button click event
+// Disabled uses the disabled prop to apply the disabled attribute to the button element

b962ddae5680779fdfb668b6554398b043a2b6ae
Jonathan Phair <jmphair@gmail.com>
2023-02-21 17:06:39 -0500

    add: stories for daylist and map over daylistitems

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,26 @@
+import React from "react";
 
+import DayListItem from "./DayListItem";
+// import "components/DayListItem.scss";
+// import classNames from "classnames";
 
-
-
+export default function DayList(props) {
+  
+  const DayListItems = props.days.map(day => {
+    return <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}/>
+  });
+  
+  return (
+    <ul>
+      {DayListItems}
+    </ul>
+  );
+}
 
 
 // PROPS NEEDED

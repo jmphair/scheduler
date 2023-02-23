9131402e05bd5b56e34b3433ea7a2f1e137ff1e1
Jonathan Phair <jmphair@gmail.com>
2023-02-22 20:06:59 -0500

    add: state and functions to form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 // import classNames from "classnames";
 
 export default function Appointment(props) {
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -152,3 +153,18 @@ storiesOf("Appointment", module)
   .add("Confirm", () => <Confirm onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", () => <Status string="Deleting"/>)
   .add("Error", () => <Error onClose={action("onClose")}/>)
+  .add("Edit", () => 
+    <Form 
+      student="Jonathan"
+      interviewer={4}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />)
+  .add("Create", () => 
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />)
+ 

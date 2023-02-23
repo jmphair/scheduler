cf0aaf206cefc57e6e78bf4eeec70b15871136fd
Jonathan Phair <jmphair@gmail.com>
2023-02-23 17:26:06 -0500

    add: new selector function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+
+
+export function getAppointmentsForDay(state, day) {
+  const correctDay = state.days.filter(days => days.name === day);
+  const appointments = [];
+
+  if (correctDay[0]) {
+    const dayIDs = correctDay[0].appointments
+
+    for (const numAppts of dayIDs) {
+      appointments.push(state.appointments[numAppts]);
+    }
+  }
+  return appointments;
+}

1c79c12e2d1b01a26fddd2ce37bb3215d1b2ae59
Jonathan Phair <jmphair@gmail.com>
2023-02-28 14:47:31 -0500

    add: testing template for appointment

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,11 +1,34 @@
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
 import React from "react";
 
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
 import { render, cleanup } from "@testing-library/react";
 
-import Application from "components/Application";
+/*
+  We import the component that we are testing
+*/
+import Appointment from "components/Appointment";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+/*
+  A test that renders a React Component
+*/
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  xit("does something it is supposed to do", () => {
+    // ...
+  });
+
+  xit("does something else it is supposed to do", () => {
+    // ...
+  });
 });

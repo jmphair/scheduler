245ab51cec5abd1c8c605afbde8604efbb793255
Jonathan Phair <jmphair@gmail.com>
2023-03-01 16:56:47 -0500

    add: tests for edit and delete

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -13,24 +13,3 @@ describe("Navigation", () => {
   })
 
 });
-
-// BOOKING
-  // Visits the root of our web server
-  // Clicks on the "Add" button in the second appointment
-  // Enters their name
-  // Chooses an interviewer
-  // Clicks the save button
-  // Sees the booked appointment
-
-// EDITING
-  // Visits the root of our web server
-  // Clicks the edit button for the existing appointment
-  // Changes the name and interviewer
-  // Clicks the save button
-  // Sees the edit to the appointment
-
-// CANCELLING
-  // Visits the root of our web server
-  // Clicks the delete button for the existing appointment
-  // Clicks the confirm button
-  // Sees that the appointment slot is empty
